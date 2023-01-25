de1fdccea6ce1bd9fb3740b3ab78f929b09e1ed6
shiam <shiamuddulla29@hotmail.com>
2023-01-25 20:42:28 +1100

    wordle project

--- script.js
+++ script.js
@@ -2,13 +2,25 @@ const keyboard = document.querySelector(".keyboard");
 const squares = document.querySelectorAll(".square")
 const keys = document.querySelectorAll(".key")
 let currentSquare = 0 
-  
+
 keyboard.addEventListener("click", (event) => {
     if(event.target.classList.contains("key")) {
-        if(currentSquare < 5) {
+        if (event.target.textContent === "ENTER") {
+            isWinner()
+        }
+        if (event.target.textContent === "DELETE") {
+            if(currentSquare > 0){
+                squares[currentSquare - 1].textContent = " "
+                currentSquare = (currentSquare - 1)
+              }
+        }
+        else if(currentSquare < 5) {
             squares[currentSquare].textContent = event.target.textContent;
             currentSquare = (currentSquare + 1)
         }
     }
 })
 
+function isWinner() {
+alert ("you have clicked")
+}
