0bf8ccd3d667688e42b0cf3cc06cc58af4a1113f
shiam <shiamuddulla29@hotmail.com>
2023-01-25 19:43:29 +1100

    made the leters stop at the 5th box

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const keyboard = document.querySelector(".keyboard");
 const squares = document.querySelectorAll(".square")
+const keys = document.querySelectorAll(".key")
 let currentSquare = 0 
-
+  
 keyboard.addEventListener("click", (event) => {
     if(event.target.classList.contains("key")) {
-        squares[currentSquare].textContent = event.target.textContent;
-        currentSquare = (currentSquare + 1)
+        if(currentSquare < 5) {
+            squares[currentSquare].textContent = event.target.textContent;
+            currentSquare = (currentSquare + 1)
+        }
     }
 })
 
