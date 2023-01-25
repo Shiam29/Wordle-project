7482678a4c99defbc1af59bc37fa3b11eae794b6
shiam <shiamuddulla29@hotmail.com>
2023-01-25 21:34:41 +1100

    wordle project

--- script.js
+++ script.js
@@ -1,26 +1,39 @@
 const keyboard = document.querySelector(".keyboard");
-const squares = document.querySelectorAll(".square")
-const keys = document.querySelectorAll(".key")
-let currentSquare = 0 
+const squares = document.querySelectorAll(".square");
+const keys = document.querySelectorAll(".key");
+const correctWords = ["S", "U", "P", "E", "R"];
+let currentSquare = 0;
+let currentRow = 0;
 
 keyboard.addEventListener("click", (event) => {
-    if(event.target.classList.contains("key")) {
+    if (event.target.classList.contains("key")) {
         if (event.target.textContent === "ENTER") {
-            isWinner()
+            for (let i = 0; i < squares.length; i++) {
+                if (i >= (currentRow * 5) && i < (currentRow * 5) + 5) {
+                    if (squares[i].textContent === correctWords[i - (currentRow * 5)]) {
+                        squares[i].style.backgroundColor = "green";
+                    } else if (correctWords.includes(squares[i].textContent)) {
+                        squares[i].style.backgroundColor = "yellow";
+                    } else {
+                        squares[i].style.backgroundColor = "grey";
+                    }
+                }
+            }
+            currentRow++;
+            currentSquare = 0;
+            for (let i = (currentRow * 5); i < (currentRow * 5) + 5; i++) {
+                squares[i].textContent = "";
+            }
         }
-        if (event.target.textContent === "DELETE") {
-            if(currentSquare > 0){
-                squares[currentSquare - 1].textContent = " "
-                currentSquare = (currentSquare - 1)
-              }
+        else if (event.target.textContent === "DELETE") {
+            if(currentSquare>0){
+                squares[(currentRow*5) + currentSquare-1].textContent = "";
+                currentSquare = (currentSquare - 1);
+            }
         }
-        else if(currentSquare < 5) {
-            squares[currentSquare].textContent = event.target.textContent;
-            currentSquare = (currentSquare + 1)
+        else if(currentSquare < 5){
+            squares[(currentRow*5) + currentSquare].textContent = event.target.textContent;
+            currentSquare = (currentSquare + 1);
         }
     }
-})
-
-function isWinner() {
-alert ("you have clicked")
-}
+});
